2ab28b4c7e471a2d7ee3d221f8c306e15908ed21
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-15 16:22:00 +0100

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,48 @@
+package com.aswtp2;
+
+public class Location {
+	
+	private double x,y;
+
+	protected Location(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	protected double getX() {
+		return x;
+	}
+
+	protected double getY() {
+		return y;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		return true;
+	}
+}
