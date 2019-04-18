3804c90d83fed1680fe26140cd935d1fed86770d
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:50:46 +0100

    Update and rename RideRole to RideRole.java

---
+++ RideRole.java
@@ -0,0 +1,20 @@
+package rsa.shared;
+
+public enum RideRole {
+	
+	DRIVER,PASSENGER;
+	
+	public RideRole other() {
+		
+	}
+	
+	public static RideRole valueOf(String name) {
+		
+	}
+	
+	public static RideRole[] values() {
+		
+	}
+	
+}
+
