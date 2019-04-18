b0b8b1e663d3be8ac63e5b7458cf7823909f627b
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:54:02 +0100

    Update UserStars.java

--- UserStars.java
+++ UserStars.java
@@ -1,1 +1,22 @@
+package rsa.shared;
+import java.io.Serializable;
+import java.lang.Comparable<UserStars>;
+
+public enum UserStars {
+
+	FIVE_STARS, FOUR_STARS, THREE_STARS, TWO_STARS, ONE_STAR;
+	
+	public int getStars() {
+		
+	}
+	
+	public static UserStars valueOf(java.lang.String name) {
+		
+	}
+	
+	public static UserStars[] values() {
+		
+	}
+	
+}
 
