e57b7510912eb5da234f27f4c89ec50d9a2bda0d
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:33:09 +0100

    Update NodeTrie.java

--- NodeTrie.java
+++ NodeTrie.java
@@ -1,1 +1,10 @@
 
+package rsa.quad;
+
+public class NodeTrie<T extends HasPoint> {
+
+	NodeTrie(double topLeftX, double topLeftY, double bottomRightX, double bottomRightY) {
+		super();
+		this.
+	}
+}
