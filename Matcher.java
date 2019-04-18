d791de43dfb3c42d015958d0e2a5eb73a06f86f3
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:37:23 +0100

    Update Matcher.java

--- Matcher.java
+++ Matcher.java
@@ -1,1 +1,50 @@
+package rsa.service;
+
+import rsa.shared.Location;
+import rsa.shared.RideMatchInfo;
+import rsa.shared.UserStars;
+
+public class Matcher {
+	
+	public void acceptMatch(long rideId, long matchId) {
+		
+	}
+	
+	public void addRide(User user, Location from, Location to,String plate, float cost) {
+		
+	}
+	
+	public void concludeRide(long rideId, UserStars stars) {
+		
+	}
+	
+	public static Location getBottomRight() {
+		
+	}
+	
+	public static double getRadius() {
+		
+	}
+	
+	public static Location getTopLeft() {
+		
+	}
+	
+	
+	public static void setBottomRight(Location bottomRight) {
+		
+	}
+	
+	public static void setRadius(double radius) {
+		
+	}
+	
+	public static void setTopLeft(Location topLeft) {
+		
+	}
+	
+	private java.util.SortedSet<RideMatchInfo> updateRide(long rideId, Location current) {
+		
+	}
+}
 
