ee5b03dae343a2d6a0b05569a548be360a445a40
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:54:45 +0100

    Update Users.java

--- Users.java
+++ Users.java
@@ -1,1 +1,39 @@
+package rsa.service;
+import java.io.File;
+
+public class Users {
+
+	protected boolean authenticate(java.lang.String nick, java.lang.String password) {
+		
+	}
+	
+	public static Users getInstance() {
+		
+	}
+	
+	
+	public static File getPlayersFile() {
+		
+	}
+	
+	public User getUser(java.lang.String nick) {
+		
+	}
+	
+	protected boolean register(java.lang.String nick, java.lang.String name, java.lang.String password) {
+		
+	}
+	
+	protected void reset() {
+		
+	}
+	
+	public static void setPlayersFile(File managerFile) {
+		
+	}
+	
+	protected boolean updatePassword(java.lang.String nick, java.lang.String oldPassword, java.lang.String newPassword) {
+		
+	}
+}
 
