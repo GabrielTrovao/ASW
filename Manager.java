79290de11320fd317e3efeea9f718b267b39f6db
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:36:50 +0100

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,15 +1,36 @@
-package com.aswtp2;
+package rsa.service;
+import rsa.quad.*;
+import rsa.shared.Location;
+import rsa.shared.PreferredMatch;
+import rsa.shared.RideMatchInfo;
+import rsa.shared.RideRole;
+import rsa.shared.RideSharingAppException;
+import rsa.shared.UserStars;
+import java.util.*;
 
 public class Manager {
 	private Users allUsers;
 	private Matcher matcher;
 	
+	// FEITO ----- CONFIRMAR (??)
 	public void acceptMatch(long rideId, long matchId) {
-		
+		Matcher newMatch = new Matcher();
+		newMatch.acceptMatch(rideId, matchId);
 	}
 	
-	public boolean authenticate(java.lang.String nick, java.lang.String password) {
+	//como é aqui para a execepçao? -- FEITO --
+	public long addRide(String nick,String password, Location from, Location to,String plate,float cost) throws RideSharingAppException {
+		User novo_User = new User(nick,null,password);
+		Ride novaRide = new Ride(novo_User, from, to, plate, cost);
+		long rideID = novaRide.getId();
+		return rideID;
+	}
+	
+	//este está mal
+	public boolean authenticate(String nick, String password) {
+		User userTest = new User();
 		
+		if(userTest.getNick()nick.equals(nick) && userTest.getPassword())
 	}
 	
 	public void concludeRide(long rideId, UserStars classification) {
@@ -29,8 +50,11 @@ public class Manager {
 	}
 	
 	public boolean register(java.lang.String nick, java.lang.String name, java.lang.String password) {
+		User user1  = new User(nick,name,password);
 		
+		return true;
 	}
+	
 	
 	private void reset() {
 		
