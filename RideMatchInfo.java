26d59ad9f5d386c8d554b6180e2da0d7a3d611c2
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:41:07 +0100

    Update RideMatchInfo.java

--- RideMatchInfo.java
+++ RideMatchInfo.java
@@ -1,1 +1,30 @@
+package rsa.shared;
+
+public class RideMatchInfo {
+
+	public Car getCar() {
+		
+	}
+
+	public float getCost() {
+		
+	}
+
+	public long getMatchId() {
+		
+	}
+	
+	public String getName(RideRole role) {
+		
+	}
+	
+	public float getStars(RideRole role) {
+		
+	}
+	
+	public Location getWhere(RideRole role) {
+		
+	}
+
+}
 
