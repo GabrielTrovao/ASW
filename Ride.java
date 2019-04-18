9fe29b778cee4b734f3547bda6c6633a5aac00e0
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:27:43 +0100

    Update Ride.java

--- Ride.java
+++ Ride.java
@@ -1,1 +1,105 @@
 
+package rsa.service;
+
+import rsa.shared.Location;
+import rsa.shared.RideMatchInfo;
+import rsa.shared.RideRole;
+
+public class Ride {
+	
+	private float cost;
+	
+	Ride(User user, Location from, Location to, java.lang.String plate, float cost){
+		
+	}
+	
+	public java.util.Comparator<RideMatchInfo> getComparator(){
+		
+	}
+	
+	public float getCost() {
+		
+	}
+	
+	public Location getCurrent() {
+		
+	}
+	
+	public Location getFrom() {
+		
+		
+	}
+
+	public long getId() {
+		
+	}
+
+	public RideMatch getMatch(){
+		
+	}
+
+	public String getPlate() {
+		
+	}
+	
+	public RideRole getRideRole() {
+		
+	}
+	
+	public Location getTo() {
+		
+	}
+	
+	public User getUser() {
+		
+	}
+	
+	public double getX() {
+		
+	}
+	
+	public double getY() {
+		
+	}
+	
+	public boolean isDriver() {
+		
+	}
+	
+	public boolean isMatched() {
+		
+	}
+	
+	public boolean isPassenger() {
+		
+	}
+	
+	public void setCost(float cost) {
+		
+	}
+	
+	public void setCurrent(Location current) {
+		
+	}
+	
+	public void setFrom(Location from) {
+		
+	}
+	
+	public void setMatch(Matcher.RideMatch match) {
+		
+	}
+	
+	public void setPlate(String plate) {
+		
+	}
+	
+	public void setTo(Location to) {
+		
+	}
+	
+	public void setUser(User user) {
+		
+	}
+	
+}
