6b131c426ea888930a98c60889eb280e8e56b583
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-18 22:51:22 +0100

    Update RideSharingAppException.java

--- RideSharingAppException.java
+++ RideSharingAppException.java
@@ -1,1 +1,21 @@
+package rsa.shared;
+
+public class RideSharingAppException {
+	
+	RideSharingAppException() {
+		
+	}
+	RideSharingAppException(java.lang.String message) {
+		
+	}
+	RideSharingAppException(java.lang.String message, java.lang.Throwable cause) {
+		
+	}
+	RideSharingAppException(java.lang.String message, java.lang.Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		
+	}
+	RideSharingAppException(java.lang.Throwable cause) {
+		
+	}
+}
 
