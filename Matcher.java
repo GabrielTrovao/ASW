891af834b03ddc58d698e292e0a6ced84ec40b77
GabrielTrovao <gabriel.trovao22@gmail.com>
2019-04-19 03:14:19 +0100

    Update Matcher.java

--- Matcher.java
+++ Matcher.java
@@ -2,16 +2,37 @@ package rsa.service;
 
 import rsa.shared.Location;
 import rsa.shared.RideMatchInfo;
+import rsa.shared.RideRole;
 import rsa.shared.UserStars;
 
 public class Matcher {
 	
+	class RideMatch {
+		protected long id;
+		protected Map<RideRole,Ride> rides;
+		
+		RideMatch(Ride lefthis.rides = 
+		}
+		
+		public long getId() {
+			return this.id;
+		}
+		
+		public Ride getRide(RideRole role) {
+			return rides.get(role);
+		
+		protected boolean matchable() {
+			
+		}
+	}
+	
 	public void acceptMatch(long rideId, long matchId) {
+		Matcher novoMatch = new Matcher();
 		
 	}
 	
 	public void addRide(User user, Location from, Location to,String plate, float cost) {
-		
+		Ride volta = new Ride(user,from,to,plate,cost);
 	}
 	
 	public void concludeRide(long rideId, UserStars stars) {
@@ -44,7 +65,7 @@ public class Matcher {
 	}
 	
 	private java.util.SortedSet<RideMatchInfo> updateRide(long rideId, Location current) {
+		setCurrent(current);
 		
 	}
 }
-
